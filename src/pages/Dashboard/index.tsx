74bd72f9a0b0417ae376600ff56af9131355559c
Tassio Rego <tassiorego@gmail.com>
2020-09-03 12:34:22 -0300

    chore: add header in Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
+import Header from '../../components/Header';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 };
 
 export default Dashboard;
