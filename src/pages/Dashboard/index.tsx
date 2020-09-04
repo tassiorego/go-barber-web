98a6feb1d91e69890f47815032d319ce0314d71d
Tassio Rego <tassiorego@gmail.com>
2020-09-04 00:39:23 -0300

    chore: add list of appointments

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,103 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 import Header from '../../components/Header';
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
       <Header />
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/16306897?s=460&u=85bef6fdb9597a9762d8463aee130366024bb032&v=4"
+                alt="Tássio Rego"
+              />
+              <strong>Tássio Rego</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/16306897?s=460&u=85bef6fdb9597a9762d8463aee130366024bb032&v=4"
+                  alt="Tássio Rego"
+                />
+                <strong>Tássio Rego</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/16306897?s=460&u=85bef6fdb9597a9762d8463aee130366024bb032&v=4"
+                  alt="Tássio Rego"
+                />
+                <strong>Tássio Rego</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/16306897?s=460&u=85bef6fdb9597a9762d8463aee130366024bb032&v=4"
+                  alt="Tássio Rego"
+                />
+                <strong>Tássio Rego</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/16306897?s=460&u=85bef6fdb9597a9762d8463aee130366024bb032&v=4"
+                  alt="Tássio Rego"
+                />
+                <strong>Tássio Rego</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -8,7 +8,7 @@ export const Header = styled.header`
 `;
 
 export const HeaderContent = styled.div`
-  max-width: 1220px;
+  max-width: 1120px;
   margin: 0 auto;
   display: flex;
   align-items: center;
@@ -55,4 +55,161 @@ export const Profile = styled.div`
       color: ${({ theme }) => theme.colors.secondary};
     }
   }
+`;
+
+export const Content = styled.main`
+  max-width: 1120px;
+  margin: 64px auto;
+  display: flex;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    display: flex;
+    margin-top: 8px;
+    align-items: center;
+    color: ${({ theme }) => theme.colors.secondary};
+
+    span {
+      display: flex;
+      align-items: center;
+    }
+
+    span + span::before {
+      content: '';
+      width: 1px;
+      height: 12px;
+      background: ${({ theme }) => theme.colors.secondary};
+      margin: 0 8px;
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    background: #3e3b47;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-top: 24px;
+    position: relative;
+
+    &::before {
+      position: absolute;
+      height: 80%;
+      width: 2px;
+      left: 0;
+      top: 10%;
+      background: ${({ theme }) => theme.colors.secondary};
+      content: '';
+    }
+  }
+
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+
+  strong {
+    margin-left: 24px;
+    color: #fff;
+  }
+
+  span {
+    margin-left: auto;
+    display: flex;
+    align-items: center;
+    color: #999591;
+
+    svg {
+      color: ${({ theme }) => theme.colors.secondary};
+      margin-right: 8px;
+    }
+  }
+`;
+
+export const Section = styled.section`
+  margin-top: 48px;
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    line-height: 26px;
+    border-bottom: 1px solid #3e3b47;
+    display: block;
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+  }
+`;
+
+export const Appointment = styled.div`
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 24px;
+  }
+
+  span {
+    margin-left: auto;
+    display: flex;
+    align-items: center;
+    color: #f4ede8;
+    svg {
+      color: ${({ theme }) => theme.colors.secondary};
+      margin-right: 8px;
+    }
+  }
+
+  div {
+    background: #3e3b47;
+    display: flex;
+    flex: 1;
+    margin-left: 24px;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    position: relative;
+
+    /* &::before {
+      position: absolute;
+      height: 80%;
+      width: 2px;
+      left: 0;
+      top: 10%;
+      background: ${({ theme }) => theme.colors.secondary};
+      content: '';
+    } */
+  }
+
+  img {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+  }
+
+  strong {
+    margin-left: 24px;
+    color: #fff;
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
 `;
