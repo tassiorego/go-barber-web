5facadfb8f192dd83654cc8389ef4a00bef50748
Tassio Rego <tassiorego@gmail.com>
2020-09-04 23:36:01 -0300

    chore: add defaults headers when start app

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -30,6 +30,7 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('GoBarber@user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return { token, user: JSON.parse(user) };
     }
     return {} as AuthState;
